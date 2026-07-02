d64cd4f2bd97d536ff4e74fa7de5af39a54cd690
csya24102-ai <csya24102@g.nihon-u.ac.jp>
2026-07-02 13:53:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'intern-selection-manager-v1';
+const CACHE_NAME = 'intern-selection-manager-v2';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
